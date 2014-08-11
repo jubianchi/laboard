9352903fd98440cbd82455dbae312a477983b19f
jubianchi <julien.bianchi@pmsipilot.com>
2014-08-11 17:44:41 +0200

    Remove cluster as it causes problem with socket.io

--- server/server.js
+++ server/server.js
@@ -1,32 +1,14 @@
 var express = require('express'),
     path = require('path'),
-    cluster = require('cluster'),
     colors = require('colors'),
     app = require('./app');
 
-if(cluster.isMaster) {
-    var cpuCount = require('os').cpus().length;
+app.logger.info('running');
 
-    app.logger.setPrefix('Master'.yellow);
-    app.logger.info('Forking ' + cpuCount + ' workers');
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 
-    for(var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
-    }
+var server = app.listen(app.config.port, function() {
+    app.logger.info('Listening on port %d', server.address().port);
+});
 
-    cluster.on('exit', function (worker) {
-        app.logger.warn('Worker ' + worker.id + ' died');
-        cluster.fork();
-    });
-} else {
-    app.logger.setPrefix(('Worker ' + cluster.worker.id).yellow);
-    app.logger.info('running');
-
-    app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
-
-    var server = app.listen(app.config.port, function() {
-        app.logger.info('Listening on port %d', server.address().port);
-    });
-
-    app.socket(server);
-}
+app.socket(server);
